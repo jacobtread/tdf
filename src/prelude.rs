87a325d8295ee06f64c0329fe8d82c57ad7d865e
Jacobtread <jacobtread@gmail.com>
2023-08-19 09:27:00 +1200

    Added errors to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,6 +2,7 @@ pub use crate::{
     codec::{
         reader::TdfReader, stringify::StringifyReader, writer::TdfWriter, Decodable, Encodable,
     },
+    error::{DecodeError, DecodeResult},
     tag::{Tag, Tagged, TdfType},
     types::*,
 };
