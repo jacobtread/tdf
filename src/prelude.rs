421759de322614311599e4126aabb7ab56e40506
Jacobtread <jacobtread@gmail.com>
2023-09-14 20:21:12 +1200

    Fixed serialize owned not included in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,7 +5,7 @@ pub use crate::stringify::{StringifyError, TdfStringifier};
 pub use crate::tag::{RawTag, Tag, Tagged, TdfType};
 pub use crate::types::{
     Blob, GroupSlice, ObjectId, ObjectType, TaggedUnion, TdfDeserialize, TdfDeserializeOwned,
-    TdfMap, TdfSerialize, TdfTyped, VarIntList, U12,
+    TdfMap, TdfSerialize, TdfSerializeOwned, TdfTyped, VarIntList, U12,
 };
 pub use crate::{reader::TdfDeserializer, writer::TdfSerializer};
 
